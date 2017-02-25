5ad91a8132507d5d6ba772437aee7a43275fb11b
jay.g <jkland927@hotmail.com>
2017-02-25 17:36:09 +0800

    add post and hook group/user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :orders
   has_many :groups
-
+  has_many :posts
   def admin?
     is_admin
   end
