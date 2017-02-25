0d6040c3510b70f36cb165b72640c966a466ef1c
jay.g <jkland927@hotmail.com>
2017-02-25 16:04:35 +0800

    show group's creator info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225065715) do
+ActiveRecord::Schema.define(version: 20170225075906) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170225065715) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "migrations", force: :cascade do |t|
