3346501ab857e8f52398b5e10d52c83fedeeafa7
jay.g <jkland927@hotmail.com>
2017-02-27 13:24:46 +0800

    change photo_env in avatar.uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,8 +5,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  if Rails.env.production?
+    storage :fog
+  elsif Rails.env.development?
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -23,7 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   version :medium do
     process resize_to_fill: [400, 400]
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
