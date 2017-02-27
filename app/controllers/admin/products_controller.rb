308e879367e6cff20e8b179010dc63c3eab47c66
jay.g <jkland927@hotmail.com>
2017-02-27 09:29:01 +0800

    multi-photos

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -16,7 +16,17 @@ class Admin::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    if @product.update(product_params)
+    if params[:photos] != nil
+      @product.photos.destroy_all #need to destroy old pics first
+
+      params[:photos]['avatar'].each do |a|
+        @picture = @product.photos.create(:avatar => a)
+      end
+
+      @product.update(product_params)
+      redirect_to admin_products_path
+
+    elsif @product.update(product_params)
       redirect_to admin_products_path
     else
       render :edit
@@ -24,12 +34,18 @@ class Admin::ProductsController < ApplicationController
   end
   def new
     @product = Product.new
+    @photo = @product.photos.build #for multi-pics
   end
 
   def create
     @product = Product.new(product_params)
 
     if @product.save
+      if params[:photos] != nil
+         params[:photos]['avatar'].each do |a|
+           @photo = @product.photos.create(:avatar => a)
+         end
+      end
       redirect_to admin_products_path
     else
       render :new
