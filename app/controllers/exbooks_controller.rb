6ef435d98346f5cd782b55115e286cbd169c642f
jay.g <jkland927@hotmail.com>
2017-03-02 15:24:22 +0800

    can not add same item to list

--- app/controllers/exbooks_controller.rb
+++ app/controllers/exbooks_controller.rb
@@ -9,8 +9,14 @@ class ExbooksController < ApplicationController
 
   def add_to_list
     @exbook = Exbook.find(params[:id])
-    current_list.add_exbook_to_list(@exbook)
-    flash[:notice] = "成功加入心愿单"
+
+    if !current_list.exbooks.include?(@exbook)
+      current_list.add_exbook_to_list(@exbook)
+      flash[:notice] = "你已成功将 《#{@exbook.title}》 加入心愿单"
+    else
+      flash[:warning] = "你的心愿单内已有此书"
+    end
+
     redirect_to :back
   end
 end
