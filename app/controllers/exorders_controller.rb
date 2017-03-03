c59cf35c565bf77f186e4038f3701167e9b3224c
jay.g <jkland927@hotmail.com>
2017-03-03 22:38:02 +0800

    Add exorder controller

---
+++ app/controllers/exorders_controller.rb
@@ -0,0 +1,21 @@
+class ExordersController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+    @exorder = Exorder.new(exorder_params)
+    @exorder.user = current_user
+    @exorder.total = current_list.total_price
+
+    if @exorder.save
+      redirect_to exorder_path(@exorder)
+    else
+      render 'lists/checkout'
+    end
+  end
+
+  private
+
+  def exorder_params
+    params.require(:exorder).permit(:shipping_name, :shipping_wechat, :shipping_phone, :shipping_location)
+  end
+end
--- app/models/exorder.rb
+++ app/models/exorder.rb
@@ -1,2 +1,8 @@
 class Exorder < ApplicationRecord
+  belongs_to :user
+
+  validates :shipping_name, presence: true
+  validates :shipping_wechat, presence: true
+  validates :shipping_phone, presence: true
+  validates :shipping_location, presence: true
 end
