ce89158fb9279398a7349cc8b87d50f68b396d26
jay.g <jkland927@hotmail.com>
2017-03-04 11:24:37 +0800

    Add exorder show function

--- app/controllers/exorders_controller.rb
+++ app/controllers/exorders_controller.rb
@@ -6,10 +6,25 @@ class ExordersController < ApplicationController
     @exorder.user = current_user
 
     if @exorder.save
+
+      current_list.list_items.each do |list_item|
+        exbook_list = ExbookList.new
+        exbook_list.exorder = @exorder
+        exbook_list.exbook_name = list_item.exbook.title
+        exbook_list.exbook_booktype = list_item.exbook.booktype
+        exbook_list.quantity = list_item.quantity
+        exbook_list.save
+      end
+
       redirect_to exorder_path(@exorder)
     else
       render 'lists/checkout'
     end
+  end
+
+  def show
+    @exorder = Exorder.find(params[:id])
+    @exbook_lists = @exorder.exbook_lists
   end
 
   private
